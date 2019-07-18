5c868db5bcfbfc2e15b6e0003b81854264834567
Matthew Maggio <techwizmatt@matthews-mbp.lan>
2019-07-17 21:56:57 -0700

    Added OBD base code

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const { app, ipcMain, BrowserWindow } = require('electron');
+var OBDReader = require('serial-obd');
+var options = {};
+options.baudRate = 38400;
+// Right sided port on Macbook Pro 2015
+var serialOBDReader = new OBDReader("/dev/tty.usbserial-1420", options);
 
 function createWindow () {
     // Create the browser window.
@@ -20,3 +25,16 @@ function createWindow () {
 }
 
 app.on('ready', createWindow);
+
+
+serialOBDReader.on('dataReceived', function (data) {
+    console.log(data);
+});
+
+serialOBDReader.on('connected', function (data) {
+    this.addPoller("rpm");
+
+    this.startPolling(100); //Polls all added pollers each 250 ms.
+});
+
+serialOBDReader.connect();

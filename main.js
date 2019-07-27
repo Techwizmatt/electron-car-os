d3b58857d17ccec2abf0a64fc5b6c50d27d8a37f
Techwizmatt <me@techwizmatt.info>
2019-07-27 02:20:14 -0700

    Testing with Python

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const { app, ipcMain, BrowserWindow } = require('electron');
-var OBDReader = require('serial-obd');
-const log = require('electron-log');
+var spawn = require('child_process').spawn;
 var os = require('os');
-var options = {};
-var obd_data = {
-    speed: 20,
-    rpm: 3454
+
+var py_obd = spawn('python', ['obd.py']);
+
+py_obd.stdout.on('data', function(data){
+    console.log(data.toString());
+});
+
+var options = {
+    baudRate : 38400
 };
-options.baudRate = 38400;
+
+var obd_data = {
+    speed: 0,
+    rpm: 0
+};
 
 var serialLocation = "/dev/serial";
 
@@ -15,8 +23,6 @@ if(os.type() == 'Darwin'){
     // Right sided port on Macbook Pro 2015
     serialLocation = "/dev/tty.usbserial-1420"
 }
-
-var serialOBDReader = new OBDReader(serialLocation, options);
 
 function createWindow () {
     // Create the browser window.
@@ -35,14 +41,6 @@ function createWindow () {
     win.loadFile('index.html');
 
     win.toggleDevTools();
-    // win.setFullScreen(true);
-    // win.setKiosk(true);
-
-    // setTimeout(function(){
-    //     //This for some reason likes to crash on RPI , So I need it in a timeout to be able to see the error.
-    //     serialOBDReader.connect();
-    // }, 1000);
-
 }
 
 app.on('ready', createWindow);
@@ -51,29 +49,6 @@ ipcMain.on('request-obd-data', (event, arg) => {
    event.sender.send('obd-data', obd_data);
 });
 
-serialOBDReader.on('dataReceived', function (data) {
-    data.forEach(function(v){
-        if('name' in v){
-            var name = v['name'];
-            var value = v['value'];
 
-            if(name == 'vss'){
-                obd_data.speed = value;
-            }
-            if(name == 'rpm'){
-                obd_data.rpm = value;
-            }
-        }
-    });
-});
 
-serialOBDReader.on('connected', function (data) {
-
-    log.info("Connected to the OBD PORT");
-
-    this.addPoller("rpm");
-    this.addPoller("vss");
-
-    this.startPolling(100); //Polls all added pollers each 250 ms.
-});
 
---
+++ obd.js
@@ -0,0 +1,5 @@
+// This is a Electron Main threaded class
+
+
+
+
